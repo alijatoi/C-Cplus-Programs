0a006932ab01c51e5d3b46c70e209b7a724a3451
Ali Hassan <alihasanjatoi@gmail.com>
2020-10-14 00:26:10 +0500

    C++ Code

---
+++ counting character how many e is in word etc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{ char ch[50],i=0;
+int count = 0;
+cout<<"Enter Any Word" ;
+cin>>ch;
+while(ch[i]!='\0'){
+
+
+if (ch[i]== 'e')
+{++count;}i++; }
+cout << count << " sa";
+
+}
